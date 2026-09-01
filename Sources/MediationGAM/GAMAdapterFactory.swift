d739b55513993ee60e18c8ec3b28e58402813007
HwanghoSong <odin.song@adpopcorn.com>
2026-09-01 10:03:38 +0900

    12110000.0.0(12.11.0.0 Adapter)배포

--- Sources/MediationGAM/GAMAdapterFactory.swift
+++ Sources/MediationGAM/GAMAdapterFactory.swift
@@ -6,7 +6,7 @@ public final class GAMAdapterFactory: NSObject, APSSPAdapterFactory {
 
     public static var networkID: Int { 19 }
     public static var sdkVersion: String? { GAMInitializationAdapter().sdkVersion }
-    public static var adapterVersion: String? { "12.10.0.0" }
+    public static var adapterVersion: String? { "12.11.0.0" }
 
     public static func makeInitializationAdapter() -> AnyObject? {
         return GAMInitializationAdapter()
