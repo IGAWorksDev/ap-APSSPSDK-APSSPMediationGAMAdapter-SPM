fac3ee74982f7f218b37bb6ee5ffa823312e6193
HwanghoSong <odin.song@adpopcorn.com>
2026-09-01 10:04:41 +0900

    13020000.0.0(13.2.0.0 Adapter)배포

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IGAWorksDev/ap-APSSPSDK-SPM.git", from: "3.4.0"),
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.1.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.2.0"),
     ],
     targets: [
         .target(name: "APSSPMediationGAM",
