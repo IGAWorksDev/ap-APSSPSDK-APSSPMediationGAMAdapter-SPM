0d99e8d55c482288a23506a41bb66dd7dc48118c
HwanghoSong <odin.song@adpopcorn.com>
2026-09-01 10:03:17 +0900

    chore: .gitignore 추가 (.build, .swiftpm, Package.resolved 제외)

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "APSSPMediationGAM",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "APSSPMediationGAM", targets: ["APSSPMediationGAM"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/IGAWorksDev/ap-APSSPSDK-SPM.git", from: "3.4.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.9.0"),
+    ],
+    targets: [
+        .target(name: "APSSPMediationGAM",
+                dependencies: [
+                    .product(name: "APSSPSDK", package: "ap-APSSPSDK-SPM"),
+                    .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                ],
+                path: "Sources/MediationGAM"),
+    ]
+)
